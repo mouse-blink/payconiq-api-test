8b708446908c4fd58d295f78c16389b3a767081f
Narek Kostanyan <83227797+narek-kostanyan-vineti@users.noreply.github.com>
2022-06-21 21:28:36 +0400

    add delete booking request and testcase for it

--- src/test/java/org/payconiq/BookingCRUDTest.java
+++ src/test/java/org/payconiq/BookingCRUDTest.java
@@ -1,6 +1,7 @@
 package org.payconiq;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,50 +24,33 @@ public class BookingCRUDTest {
 
     private static RESTClient client;
 
+    private static Booking minimalBooking = Booking.builder()
+            .firstname("")
+            .lastname("")
+            .totalprice(0)
+            .depositpaid(true)
+            .bookingdates(new Dates("1970-01-01", "2038-01-19"))
+            .additionalneeds("")
+            .build();
+
     @BeforeAll
-    static void prepareRESTClient() {
+    static void prepareRESTClient() throws UnirestException {
         client = new RESTClient(
                 "https://restful-booker.herokuapp.com",
                 new Credentials("admin", "password123")
         );
+        client.authenticate();
     }
 
-    @Test
-    @DisplayName("It is able to crate booking")
-    void crateBooking() throws UnirestException {
-        Booking booking = Booking.builder()
-                .firstname("Jim")
-                .lastname("Brown")
-                .totalprice(111)
-                .depositpaid(true)
-                .bookingdates(new Dates("1970-01-01", "2038-01-19"))
-                .additionalneeds("Breakfast")
-                .build();
-        BookingResource bookingResource = client.createBooking(booking);
-        assertAll(
-                () -> assertNotNull(bookingResource.getBookingid()),
-                () -> BookingAssertions.assetEquals(bookingResource.getBooking(), booking)
-        );
-        Booking gottenBooking = client.getBooking(bookingResource.getBookingid());
-        BookingAssertions.assetEquals(gottenBooking,booking);
-    }
 
     @Test
     @DisplayName("It is able to crate booking with minimal values")
     void crateBookingWithMinimalValues() throws UnirestException {
         BookingResource[] initialBookingResources = client.getBookingIds();
-        Booking booking = Booking.builder()
-                .firstname("")
-                .lastname("")
-                .totalprice(0)
-                .depositpaid(true)
-                .bookingdates(new Dates("1970-01-01", "2038-01-19"))
-                .additionalneeds("")
-                .build();
-        BookingResource bookingResource = client.createBooking(booking);
+        BookingResource bookingResource = client.createBooking(minimalBooking);
         assertAll(
                 () -> assertNotNull(bookingResource.getBookingid()),
-                () -> BookingAssertions.assetEquals(bookingResource.getBooking(), booking)
+                () -> BookingAssertions.assetEquals(bookingResource.getBooking(), minimalBooking)
         );
         List<BookingResource> filteredInitialBookingResources = Arrays.stream(initialBookingResources)
                 .filter(res -> res.getBookingid() == bookingResource.getBookingid())
@@ -79,6 +63,19 @@ public class BookingCRUDTest {
         assertEquals(filteredBookingResources.size(),1);
         log.info("Getting Booking by ID : "+bookingResource.getBookingid());
         Booking gottenBooking = client.getBooking(bookingResource.getBookingid());
-        BookingAssertions.assetEquals(gottenBooking,booking);
+        BookingAssertions.assetEquals(gottenBooking,minimalBooking);
+    }
+
+    @Test
+    @DisplayName("It is able to Delete booking by id")
+    public void deleteBooking() throws UnirestException {
+        BookingResource bookingResource = client.createBooking(minimalBooking);
+        String result = client.deleteBooking(bookingResource.getBookingid());
+        assertEquals(result,"Created");
+        BookingResource[] bookingResources = client.getBookingIds();
+        List<BookingResource> filteredBookingResources = Arrays.stream(bookingResources)
+                .filter(res -> res.getBookingid() == bookingResource.getBookingid())
+                .collect(Collectors.toList());
+        assertEquals(filteredBookingResources.size(),0);
     }
 }
